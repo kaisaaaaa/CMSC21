cbbec7dd7e61691604849da6c4f33561a2b9fffa
kaisaaaaa <130482573+kaisaaaaa@users.noreply.github.com>
2023-06-22 12:24:37 +0800

    Add files via upload

---
+++ Lecture11/anagram2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+void scan_word(int* occurrences) {
+    char c;
+    while ((c = getchar()) != '\n') {                               // read characters until a newline
+        if (isdigit(c)) {                                           // to check if the input is a digit
+            printf("You have entered a number. Please enter a word.\n");
+            while ((c = getchar()) != '\n') {                       // read and discard the remaining characters until a new line 
+            }
+            return scan_word(occurrences);                          // ask for user input again
+        } else if (isalpha(c)) {                                    // to check if it is a letter
+            occurrences[toupper(c) - 'A']++;                        // count increment
+        }
+    }
+}
+
+bool is_anagram(int* occurrences1, int* occurrences2) {
+    for (int i = 0; i < 26; i++) {
+        if (occurrences1[i] != occurrences2[i]) {                   // compare the two occurences
+            return false;                                           // not equal means not an anagram
+        }
+    }
+    return true;                                                    // anagram if all counts match
+}
+
+int main(void) {
+    int occurrences1[26] = {0};                                     // initialize array at 0
+    int occurrences2[26] = {0};                                     // initialize array at 0
+
+    printf("Enter first word: ");
+    scan_word(occurrences1);                                        
+
+    printf("Enter second word: ");
+    scan_word(occurrences2);                                        
+
+    if (is_anagram(occurrences1, occurrences2)) {
+        printf("The words are anagrams.\n");
+    } else {
+        printf("The words are not anagrams.\n");
+    }
+
+    return 0;
+}
